6ead67b3851684579f52f19ea66c79b791f2d17e
himalsamarawickrama <himaluni1@gmail.com>
2025-03-01 16:09:32 +0530

    Delete LW01/src/Main.java

--- LW01/src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class Main {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Welcome to Java");
-        frame.setVisible(true);
-        frame.setSize(800, 600);
-    }
-
-}
